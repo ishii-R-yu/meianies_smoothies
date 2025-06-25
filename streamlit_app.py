eb229bab80a0bca59c4b8edcbacd1ae3d7846f2a
ishii-R-yu <ishii@macro-send.com>
2025-06-25 18:08:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,10 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-  
-    st.write(ingredients_string)
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER,ORDER_FILLED)
-            values ('""" + ingredients_string + """','""" + name_on_order + """','False')"""
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')  
+        st.write(ingredients_string)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER,ORDER_FILLED)
+                values ('""" + ingredients_string + """','""" + name_on_order + """','False')"""
 
     time_to_insert = st.button('Submit Order')
 
