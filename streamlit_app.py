fc6b3c76a8468356eb9069031d4709c1b6285fad
ishii-R-yu <ishii@macro-send.com>
2025-06-25 18:21:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,14 +50,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-
-import requests
-
-
-if ingredients_list:
-    ingredients_string = ''
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ''
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
-        st_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
