c70fd4a5767e9f72584d0aa1cfb1c71f96f6b79e
ishii-R-yu <ishii@macro-send.com>
2025-06-25 18:20:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.') 
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvicefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
-        fv_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+        fv_df = st.dataframe(data=fruityvicefroot_response.json(),use_container_width=True)
 
     time_to_insert = st.button('Submit Order')
 
